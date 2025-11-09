05965b19210ddb0aa821d4e049fd6cdf17c44332
אבישי1 <avishai1.bardenstein@gmail.com>
2025-11-09 23:57:00 +0200

    9.11 home

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,10 +4,23 @@ namespace Chess
 {
     public partial class MainPage : ContentPage
     {
+        private readonly MainPageVm mpVM = new();
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = new MainPageVm();
-        }       
-    }
+            BindingContext = mpVM;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            mpVM.AddSnapshotListener();
+        }
+
+        protected override void OnDisappearing()
+        {
+            mpVM.RemoveSnapshotListener();
+            base.OnDisappearing();
+        }
+    }       
+    
 }
